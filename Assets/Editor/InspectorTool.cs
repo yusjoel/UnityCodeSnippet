db593a8d30eff08e2fabf2e08137b53703621633
yusjoel <joelyusj@hotmail.com>
2018-11-15 11:15:19 +0800

    add InspectorTool.cs add ProjectSettingsInspector.cs

---
+++ Assets/Editor/InspectorTool.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class InspectorTool : MonoBehaviour
+{
+    /// <summary>
+    ///     在Project窗口打开新的Inspector窗口
+    /// </summary>
+    [MenuItem("Assets/New Inspector")]
+    private static void NewAssetInspector()
+    {
+        if (Selection.activeObject != null)
+            InspectTarget(Selection.activeObject);
+    }
+
+    /// <summary>
+    ///     在Hierarchy窗口打开新的Inspector窗口
+    /// </summary>
+    [MenuItem("GameObject/New Inspector", false, 11)]
+    private static void NewGameObjectInspector()
+    {
+        if (Selection.activeGameObject != null)
+            InspectTarget(Selection.activeGameObject);
+    }
+
+    // https://answers.unity.com/questions/36131/editor-multiple-inspectors.html
+    // author: vexe
+
+    /// <summary>
+    ///     Creates a new inspector window instance and locks it to inspect the specified target
+    /// </summary>
+    public static void InspectTarget(Object target)
+    {
+        // Get a reference to the `InspectorWindow` type object
+        var inspectorType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+        // Create an InspectorWindow instance
+        var inspectorInstance = ScriptableObject.CreateInstance(inspectorType) as EditorWindow;
+        // We display it - currently, it will inspect whatever gameObject is currently selected
+        // So we need to find a way to let it inspect/aim at our target GO that we passed
+        // For that we do a simple trick:
+        // 1- Cache the current selected gameObject
+        // 2- Set the current selection to our target GO (so now all inspectors are targeting it)
+        // 3- Lock our created inspector to that target
+        // 4- Fallback to our previous selection
+        inspectorInstance.Show();
+        // Cache previous selected gameObject
+        var prevSelection = Selection.activeObject;
+        // Set the selection to GO we want to inspect
+        Selection.activeObject = target;
+        // Get a ref to the "locked" property, which will lock the state of the inspector to the current inspected target
+        var isLocked = inspectorType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.Public);
+        // Invoke `isLocked` setter method passing 'true' to lock the inspector
+        isLocked.GetSetMethod().Invoke(inspectorInstance, new object[] {true});
+        // Finally revert back to the previous selection so that other inspectors continue to inspect whatever they were inspecting...
+        Selection.activeObject = prevSelection;
+    }
+}
---
+++ Assets/Editor/ProjectSettingsInspector.cs
@@ -0,0 +1,156 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+class ProjectSettingsInspector : EditorWindow
+{
+    //
+    // Written by Peter Schraut
+    //     http://www.console-dev.de
+    //
+    // The "Project Settings Inspector" window embeds all Unity project settings
+    // in a single window.
+    //
+    // Store this file in your Unity project in a folder called "Editor".
+    // You can open this "Project Settings Inspector" from Unity's main menu
+    //     Mainmenu > Edit > Project Settings > Everything
+    //
+    // Download most recent version from:
+    //     https://bitbucket.org/snippets/pschraut/5edXM8
+    //
+    Vector2 m_ScrollView;
+
+    class Binding
+    {
+        public Editor editor;
+        public string objectName;
+        public float minHeight;
+
+        public Binding(string objectName, float minSize)
+        {
+            this.objectName = objectName;
+            this.minHeight = minSize;
+        }
+    }
+
+    // These are the asset names of those project settings objects as found in memory.
+    static Binding[] s_Bindings = new Binding[]
+    {
+        new Binding("InputManager", 0),
+        new Binding("TagManager", 0),
+        new Binding("AudioManager", 0),
+        new Binding("TimeManager",  0),// Not in ProjectSettings folder
+        new Binding("ProjectSettings", 0),
+        new Binding("PlayerSettings", 0),
+        new Binding("PhysicsManager",  800),// DynamicsManager.asset
+        new Binding("Physics2DSettings", 1000),
+        new Binding("QualitySettings", 0),
+        new Binding("GraphicsSettings", 0),
+        new Binding("NetworkManager", 0),
+        new Binding("Editor Settings",  0),// EditorSettings.asset
+        new Binding("MonoManager", 600), // Not in ProjectSettings folder (Script Execution Order)
+    };
+
+
+    void OnEnable()
+    {
+        titleContent = new GUIContent("ProjectSettings");
+
+        var unityEngine = typeof(UnityEngine.Time).Assembly;
+        var unityEditor = typeof(UnityEditor.EditorGUI).Assembly;
+        var allObjects = Resources.FindObjectsOfTypeAll<UnityEngine.Object>();
+
+        foreach (var binding in s_Bindings)
+        {
+            foreach (var obj in allObjects)
+            {
+                if (obj == null)
+                    continue;
+
+                // The name of the asset is usually the filename as well
+                if (!string.Equals(obj.name, binding.objectName, System.StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                // Only consider Unity classes
+                var assembly = obj.GetType().Assembly;
+                if (assembly != unityEngine && assembly != unityEditor)
+                    continue;
+
+                // NetworkManager was found 3 times, so filter out the 2 of type 'MonoScript'
+                // Uses this log to figure it out:
+                // Debug.Log(obj.name + ", Type: " + obj.GetType().FullName + ", Assembly: " + obj.GetType().Assembly.FullName);
+                if (obj.GetType() == typeof(MonoScript))
+                    continue;
+
+                binding.editor = Editor.CreateEditor(obj);
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        for (var n = 0; n < s_Bindings.Length; ++n)
+        {
+            if (s_Bindings[n] != null)
+            {
+                Editor.DestroyImmediate(s_Bindings[n].editor);
+                s_Bindings[n].editor = null;
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        using (var scrollView = new EditorGUILayout.ScrollViewScope(m_ScrollView))
+        {
+            m_ScrollView = scrollView.scrollPosition;
+
+            foreach (var binding in s_Bindings)
+            {
+                if (binding.editor == null)
+                    continue;
+
+                var editor = binding.editor;
+                editor.DrawHeader();
+
+                // We use the header rect as a clickable button for later.
+                // However, the header has buttons itself on the right side,
+                // so we adjust our rectangle to be smaller to not mess with the default buttons.
+                var headerRect = GUILayoutUtility.GetLastRect();
+                headerRect.width *= 0.75f;
+
+                // Use a guid to avoid name clashes
+                var editorPrefsKey = "daf29d3b-0aca-45c6-b529-df6c834be417/" + editor.target.name;
+                var isExpanded = EditorPrefs.GetBool(editorPrefsKey, false);
+
+                // Draw an invisble button over the header
+                if (GUI.Button(headerRect, "", GUI.skin.label))
+                {
+                    EditorPrefs.SetBool(editorPrefsKey, !isExpanded);
+                    continue;
+                }
+
+                if (!isExpanded)
+                    continue;
+
+                // Draw the editor
+                using (new EditorGUILayout.VerticalScope(GUILayout.MinHeight(binding.minHeight)))
+                {
+                    editor.OnInspectorGUI();
+                }
+            }
+        }
+    }
+
+    [MenuItem("Edit/Project Settings/Everything", priority = 0)]
+    static void OpenWindowMenuItem()
+    {
+        var wnd = GetWindow<ProjectSettingsInspector>();
+        if (wnd != null)
+            wnd.Show();
+    }
+}
+#endif
