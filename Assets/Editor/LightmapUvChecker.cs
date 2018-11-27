4d3eb055dac00d4b436611afccc1c50eafe13cf1
yusjoel <joelyusj@hotmail.com>
2018-11-27 12:58:26 +0800

    add LightmapUvChecker

---
+++ Assets/Editor/LightmapUvChecker.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+// (srcData.GetChannelMask() & copyChannels) == copyChannels
+// issue: https://issuetracker.unity3d.com/issues/assertion-srcinfo-dot-getchannelmask-and-copychannels-equals-equals-copychannels-after-marking-mesh-without-lightmaps-uvs-as-lightmap-static
+// question: https://answers.unity.com/questions/1470570/getting-some-error-always-when-i-open-unity.html
+// RaymondEllis:
+// > Apparently this is an issue with meshes without UV being set as lightmap static.
+// > Simple fix: for every static object in your scene, uncheck "Lightmap Static".
+// ThePilgrim: 
+// > I had this same error. I had a static mesh without UVs in my scene. To fix it, I went to the import settings on that mesh and checked "Generate Lightmap UVs". 
+
+public class LightmapUvChecker : EditorWindow
+{
+    private readonly Dictionary<string, List<MeshFilter>> modelPathMeshesMap = new Dictionary<string, List<MeshFilter>>();
+    private int countOfMeshFilter;
+    private int countOfModel;
+    private Vector2 scrollPosition;
+    private bool willRefresh;
+
+    [MenuItem("Tools/Lightmap UV Checker")]
+    private static void Create()
+    {
+        var me = GetWindow<LightmapUvChecker>();
+        me.Load();
+    }
+
+    private void AddMeshFilter(MeshFilter meshFilter)
+    {
+        var mesh = meshFilter.sharedMesh;
+        string guid;
+        int localId;
+        string assetPath = "(N/A)";
+        if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(mesh, out guid, out localId)) assetPath = AssetDatabase.GUIDToAssetPath(guid);
+
+        if (!modelPathMeshesMap.ContainsKey(assetPath))
+        {
+            modelPathMeshesMap[assetPath] = new List<MeshFilter>();
+            if (assetPath != "(N/A)")
+                countOfModel++;
+        }
+
+        modelPathMeshesMap[assetPath].Add(meshFilter);
+        countOfMeshFilter++;
+    }
+
+    private void Load()
+    {
+        modelPathMeshesMap.Clear();
+        countOfMeshFilter = 0;
+        countOfModel = 0;
+
+        var meshFilters = FindObjectsOfType<MeshFilter>();
+        foreach (var meshFilter in meshFilters)
+        {
+            var mesh = meshFilter.sharedMesh;
+            if (mesh == null)
+                continue;
+
+            var flags = GameObjectUtility.GetStaticEditorFlags(meshFilter.gameObject);
+            bool isLightmapStatic = (flags & StaticEditorFlags.LightmapStatic) != 0;
+            if (isLightmapStatic && IsNullOrEmpty(mesh.uv) && IsNullOrEmpty(mesh.uv2))
+                AddMeshFilter(meshFilter);
+        }
+    }
+
+    private bool IsNullOrEmpty<T>(T[] array)
+    {
+        if (array == null) return true;
+        if (array.Length == 0) return true;
+        return false;
+    }
+
+    private void OnGUI()
+    {
+        if (willRefresh)
+        {
+            willRefresh = false;
+            Load();
+        }
+
+        DrawTitle();
+
+        GUILayout.Space(10);
+
+        DrawContent();
+    }
+
+    private void DrawContent()
+    {
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+        foreach (var pair in modelPathMeshesMap)
+        {
+            GUILayout.BeginHorizontal();
+
+            string assetPath = pair.Key;
+            var model = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+            if (model)
+                EditorGUILayout.ObjectField(model.name, model, typeof(GameObject), true);
+            else
+                GUILayout.Label("(N/A)");
+
+            var meshFilters = pair.Value;
+            if (GUILayout.Button("Remove Lightmap Static")) RemoveLightmapStatic(meshFilters);
+
+            if (GUILayout.Button("Generate Lightmap UVs")) GenerateLightmapUvs(assetPath);
+
+            GUILayout.EndHorizontal();
+
+            foreach (var meshFilter in meshFilters)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Space(20);
+                EditorGUILayout.ObjectField(meshFilter.name, meshFilter, typeof(MeshFilter), true);
+                var mesh = meshFilter.sharedMesh;
+                EditorGUILayout.ObjectField(mesh.name, mesh, typeof(Mesh), true);
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        GUILayout.EndScrollView();
+    }
+
+    private void DrawTitle()
+    {
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Refresh"))
+            Load();
+
+        if (GUILayout.Button("Remove Lightmap Static"))
+            foreach (var pair in modelPathMeshesMap)
+            {
+                var meshFilters = pair.Value;
+                RemoveLightmapStatic(meshFilters);
+            }
+
+        if (GUILayout.Button("Generate Lightmap UVs"))
+            foreach (var pair in modelPathMeshesMap)
+            {
+                string assetPath = pair.Key;
+                GenerateLightmapUvs(assetPath);
+            }
+
+        GUILayout.Label(string.Format("Model: {0} / Mesh: {1}", countOfModel, countOfMeshFilter));
+
+        GUILayout.EndHorizontal();
+    }
+
+    private void GenerateLightmapUvs(string assetPath)
+    {
+        var modelImporter = AssetImporter.GetAtPath(assetPath) as ModelImporter;
+        if (modelImporter == null)
+            return;
+        modelImporter.generateSecondaryUV = true;
+        modelImporter.SaveAndReimport();
+        willRefresh = true;
+    }
+
+    private void RemoveLightmapStatic(List<MeshFilter> meshFilters)
+    {
+        foreach (var meshFilter in meshFilters)
+        {
+            var go = meshFilter.gameObject;
+            var flags = GameObjectUtility.GetStaticEditorFlags(go);
+            var newFlags = flags & ~StaticEditorFlags.LightmapStatic;
+            //Debug.LogFormat(go, "Old Flags:{0}\nNew Flags: {1}", flags, newFlags);
+            GameObjectUtility.SetStaticEditorFlags(go, newFlags);
+            EditorSceneManager.MarkSceneDirty(go.scene);
+            willRefresh = true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -12,3 +12,7 @@
 在Edit - Project Settings的子菜单中增加一项Everything, 点击后创建一个锁定的新窗口, 可以编辑所有的工程设置.
 * [Thread on Unity Forum](https://forum.unity.com/threads/new-settings-gui.557308/)
 * [Snippet on Bitbucket](https://bitbucket.org/snippets/pschraut/5edXM8/unity-all-project-settings-in-a-single)
+
+### LightmapUvChecker.cs
+当一个物件没有UV时, 又被勾选了Lightmap Static, 会报错: (srcData.GetChannelMask() & copyChannels) == copyChannels. 这个工具就是为了修正这个问题.
+* [Question on Unity Answers](https://answers.unity.com/questions/1470570/getting-some-error-always-when-i-open-unity.html)
