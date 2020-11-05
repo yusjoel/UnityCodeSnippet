91120d950bc29c0d501e3fa05c0ad0af2b9ac89d
Joel Yu <joelyusj@hotmail.com>
2020-11-05 17:46:37 +0800

    编辑器功能: 截屏

---
+++ Assets/Editor/ScreenCaptureTool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public static class ScreenCaptureTool
+{
+    [MenuItem("Tools/截屏")]
+    static void CaptureScreenshot()
+    {
+        string filename = string.Format("screenshot_{0:yyyyMMddhhmmss}.png", DateTime.Now);
+        ScreenCapture.CaptureScreenshot(filename);
+    }
+}
